41e138352004127cfa3405022477cf4b4e01cf1a
Aaryan Pradhan <pradhanaaryan@gmail.com>
2024-12-15 12:07:22 +0530

    database added

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+
+def init_db():
+    conn = sqlite3.connect("messages.db")
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            message TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
+init_db()
 
 @app.route('/')
 def home():
@@ -10,15 +28,19 @@ def home():
 def about():
     return render_template('about.html')
 
-
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
+        
         name = request.form['name']
         message = request.form['message']
+
         
-        with open('messages.txt', 'a') as file:
-            file.write(f"Name:{name}\nMessage: {message}\n{'-'*50}\n")
+        conn = sqlite3.connect("messages.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO messages (name, message) VALUES (?, ?)", (name, message))
+        conn.commit()
+        conn.close()
 
         return f"<h1>Thanks, {name}!</h1><p>Your message has been saved.</p><a href='/'>Back to Home</a>"
 
@@ -26,13 +48,22 @@ def contact():
 
 @app.route('/messages')
 def messages():
-    with open('messages.txt',"r") as file:
-        messages = file.readlines()
-    return render_template('messages.html', messages=messages)
+    conn = sqlite3.connect("messages.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, message FROM messages")
+    all_messages = cursor.fetchall()
+    conn.close()
+
+    return render_template('messages.html', messages=all_messages)
 
 @app.route('/clear-messages', methods=['POST'])
 def clear_messages():
-    open("messages.txt", "w").close()
+    conn = sqlite3.connect("messages.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM messages")
+    conn.commit()
+    conn.close()
+
     return redirect(url_for('messages'))
 
 if __name__ == '__main__':
