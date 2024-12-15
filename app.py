cfc758241a4830ed016a6a46968d5abebe72f85d
Aaryan Pradhan <pradhanaaryan@gmail.com>
2024-12-15 11:36:07 +0530

    contact page added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        return f"<h1>Thanks, {name}!</h1><p>Your message: {message}</p><a href='/'>Back to Home</a>"
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
